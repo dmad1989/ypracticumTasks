1ef17e22178fb0a506edfa634b10f45586a7ef82
dmad1989 <dmad1989@gmail.com>
2024-01-05 22:22:05 +0300

    sprint 1 incr3_2 task2 carService init commit

---
+++ sprint1/incr3_2/task2/go.mod
@@ -0,0 +1,3 @@
+module github.com/dmad1989/ypracticumTasks/sprint1/incr3_2/task2
+
+go 1.21.3
---
+++ sprint1/incr3_2/task2/main.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"io"
+	"log"
+	"net/http"
+	"strings"
+)
+
+var cars = map[string]string{
+	"id1": "Renault Logan",
+	"id2": "Renault Duster",
+	"id3": "BMW X6",
+	"id4": "BMW M5",
+	"id5": "VW Passat",
+	"id6": "VW Jetta",
+	"id7": "Audi A4",
+	"id8": "Audi Q7",
+}
+
+// carsListFunc — вспомогательная функция для вывода всех машин.
+func carsListFunc() []string {
+	var list []string
+	for _, c := range cars {
+		list = append(list, c)
+	}
+	return list
+}
+
+// carFunc — вспомогательная функция для вывода определённой машины.
+func carFunc(id string) string {
+	if c, ok := cars[id]; ok {
+		return c
+	}
+	return "unknown identifier " + id
+}
+
+func carsHandle(rw http.ResponseWriter, r *http.Request) {
+	carsList := carsListFunc()
+	io.WriteString(rw, strings.Join(carsList, ", "))
+}
+
+func carHandle(rw http.ResponseWriter, r *http.Request) {
+	carID := r.URL.Query().Get("id")
+	if carID == "" {
+		http.Error(rw, "carID param is missed", http.StatusBadRequest)
+		return
+	}
+	rw.Write([]byte(carFunc(carID)))
+}
+
+func main() {
+	// определяем хендлер, который выводит все машины
+	http.HandleFunc("/cars", carsHandle)
+	// определяем хендлер, который выводит определённую машину
+	http.HandleFunc("/car", carHandle)
+
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
