960071e43d25981129e3f5c4c3418c6d15ff9b5a
dmad1989 <dmad1989@gmail.com>
2024-01-13 12:28:19 +0300

    feat add task5

---
+++ sprint1/incr4/task5/go.mod
@@ -0,0 +1,5 @@
+module github.com/dmad1989/ypracticumTasks/sprint1/incr4/task5
+
+go 1.21.3
+
+require github.com/spf13/pflag v1.0.5 // indirect
---
+++ sprint1/incr4/task5/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"os"
+)
+
+var version = "0.0.1"
+
+func main() {
+	// допишите код
+	flag.Usage = func() {
+		fmt.Fprintf(flag.CommandLine.Output(), "Version: %v\nUsage of %s:\n", version, os.Args[0])
+		flag.PrintDefaults()
+	}
+
+	flag.Parse()
+}
