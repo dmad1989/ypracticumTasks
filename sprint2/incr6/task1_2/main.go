0e1c5ccea0d971dd0d9867904c9fcc219e5fe4bc
dmad1989 <dmad1989@gmail.com>
2024-01-20 12:52:37 +0300

    add log request

--- sprint2/incr6/task1_2/main.go
+++ sprint2/incr6/task1_2/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"time"
 
 	"go.uber.org/zap"
 )
@@ -21,7 +22,7 @@ func main() {
 	// делаем регистратор SugaredLogger
 	sugar = *logger.Sugar()
 
-	http.Handle("/ping", pingHandler())
+	http.Handle("/ping", WithLogging(pingHandler()))
 
 	addr := "127.0.0.1:8080"
 	// записываем в лог, что сервер запускается
@@ -42,4 +43,36 @@ func pingHandler() http.Handler {
 		_, _ = fmt.Fprintf(w, "pong\n")
 	}
 	return http.HandlerFunc(fn)
+}
+
+// WithLogging добавляет дополнительный код для регистрации сведений о запросе
+// и возвращает новый http.Handler.
+func WithLogging(h http.Handler) http.Handler {
+	logFn := func(w http.ResponseWriter, r *http.Request) {
+		// функция Now() возвращает текущее время
+		start := time.Now()
+
+		// эндпоинт /ping
+		uri := r.RequestURI
+		// метод запроса
+		method := r.Method
+
+		// точка, где выполняется хендлер pingHandler
+		h.ServeHTTP(w, r) // обслуживание оригинального запроса
+
+		// Since возвращает разницу во времени между start
+		// и моментом вызова Since. Таким образом можно посчитать
+		// время выполнения запроса.
+		duration := time.Since(start)
+
+		// отправляем сведения о запросе в zap
+		sugar.Infoln(
+			"uri", uri,
+			"method", method,
+			"duration", duration,
+		)
+
+	}
+	// возвращаем функционально расширенный хендлер
+	return http.HandlerFunc(logFn)
 }
