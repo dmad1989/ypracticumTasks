3dafed41075e17014c80bd8dbe6d743c1f0fa067
dmad1989 <dmad1989@gmail.com>
2024-01-20 13:04:24 +0300

    add reponse logging

--- sprint2/incr6/task1_2/main.go
+++ sprint2/incr6/task1_2/main.go
@@ -51,14 +51,22 @@ func WithLogging(h http.Handler) http.Handler {
 	logFn := func(w http.ResponseWriter, r *http.Request) {
 		// функция Now() возвращает текущее время
 		start := time.Now()
+		responseData := &responseData{
+			status: 0,
+			size:   0,
+		}
 
+		lw := loggingResponseWriter{
+			ResponseWriter: w, // встраиваем оригинальный http.ResponseWriter
+			responseData:   responseData,
+		}
 		// эндпоинт /ping
 		uri := r.RequestURI
 		// метод запроса
 		method := r.Method
 
 		// точка, где выполняется хендлер pingHandler
-		h.ServeHTTP(w, r) // обслуживание оригинального запроса
+		h.ServeHTTP(&lw, r) // обслуживание оригинального запроса
 
 		// Since возвращает разницу во времени между start
 		// и моментом вызова Since. Таким образом можно посчитать
@@ -72,7 +80,42 @@ func WithLogging(h http.Handler) http.Handler {
 			"duration", duration,
 		)
 
+		sugar.Infoln(
+			"uri", r.RequestURI,
+			"method", r.Method,
+			"status", responseData.status, // получаем перехваченный код статуса ответа
+			"duration", duration,
+			"size", responseData.size, // получаем перехваченный размер ответа
+		)
+
 	}
 	// возвращаем функционально расширенный хендлер
 	return http.HandlerFunc(logFn)
+}
+
+type (
+	// берём структуру для хранения сведений об ответе
+	responseData struct {
+		status int
+		size   int
+	}
+
+	// добавляем реализацию http.ResponseWriter
+	loggingResponseWriter struct {
+		http.ResponseWriter // встраиваем оригинальный http.ResponseWriter
+		responseData        *responseData
+	}
+)
+
+func (r *loggingResponseWriter) Write(b []byte) (int, error) {
+	// записываем ответ, используя оригинальный http.ResponseWriter
+	size, err := r.ResponseWriter.Write(b)
+	r.responseData.size += size // захватываем размер
+	return size, err
+}
+
+func (r *loggingResponseWriter) WriteHeader(statusCode int) {
+	// записываем код статуса, используя оригинальный http.ResponseWriter
+	r.ResponseWriter.WriteHeader(statusCode)
+	r.responseData.status = statusCode // захватываем код статуса
 }
