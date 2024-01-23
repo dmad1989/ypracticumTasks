d1f30f57aaa2eda493b7b770b1ba52832a4b7819
dmad1989 <dmad1989@gmail.com>
2024-01-23 23:25:53 +0300

    add gob task

---
+++ sprint2/incr7/task4_2/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"bytes"
+	"encoding/gob"
+	"fmt"
+)
+
+func main() {
+	// data содержит данные в формате gob
+	data := []byte{12, 255, 129, 2, 1, 2, 255, 130, 0, 1, 12,
+		0, 0, 17, 255, 130, 0, 2, 6, 72, 101, 108, 108,
+		111, 44, 5, 119, 111, 114, 108, 100}
+
+	// напишите код, который декодирует data в массив строк
+	// 1. создайте буфер `bytes.NewBuffer(data)` для передачи в декодер
+	buf := bytes.NewBuffer(data)
+	// 2. создайте декодер `dec := gob.NewDecoder(buf)`
+	dec := gob.NewDecoder(buf)
+	// 3. определите `make([]string, 0)` для получения декодированного слайса
+	resSlice := make([]string, 0)
+	// 4. декодируйте данные используя функцию `dec.Decode`
+	if err := dec.Decode(&resSlice); err != nil {
+		panic(err)
+	}
+	// ...
+	fmt.Println(resSlice)
+}
