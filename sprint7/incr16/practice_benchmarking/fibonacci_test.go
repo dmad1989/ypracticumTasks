7c3e641433d85f6023f6daed1d51e99e3fef51da
dmad1989 <dmad1989@gmail.com>
2024-04-14 12:58:28 +0300

    add becnchmarksor slice with b.timer + banch task

--- sprint7/incr16/practice_benchmarking/fibonacci_test.go
+++ sprint7/incr16/practice_benchmarking/fibonacci_test.go
@@ -1,6 +1,11 @@
 package main
 
-import "testing"
+import (
+	"math/rand"
+	"sort"
+	"testing"
+	"time"
+)
 
 func BenchmarkFibo(b *testing.B) {
 	count := 20
@@ -15,4 +20,25 @@ func BenchmarkFibo(b *testing.B) {
 			FiboOptimized(count)
 		}
 	})
+}
+
+func BenchmarkSortSlice(b *testing.B) {
+	rand.Seed(time.Now().UnixNano())
+	slice := make([]int, 10000)
+	b.ResetTimer() // сбрасываем счётчик, чтобы инициализация слайса не посчиталась
+
+	var cmps int64
+	for i := 0; i < b.N; i++ {
+		b.StopTimer()
+		for i := 0; i < len(slice); i++ {
+			slice[i] = rand.Intn(1000)
+		}
+		b.StartTimer()
+		// сортируем
+		sort.Slice(slice, func(i, j int) bool {
+			cmps++ // увеличиваем счётчик
+			return slice[i] < slice[j]
+		})
+	}
+	b.ReportMetric(float64(cmps)/float64(b.N), "compares/op")
 }
---
+++ sprint7/incr16/task6_2/go.mod
@@ -0,0 +1,3 @@
+module C/WorkFolder/ypracticumTasks/sprint7/incr16/task6_2
+
+go 1.21.3
